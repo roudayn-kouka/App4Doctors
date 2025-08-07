a17e0978baa6ed7173e5c640efe56eb799fbd96a
roudayna-kouka <roudayna.kouka@etudiant-fst.utm.tn>
2025-08-08 00:31:21 +0100

    integration backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Servir les fichiers statiques du frontend (build Vite)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on http://localhost:5000");
+});
